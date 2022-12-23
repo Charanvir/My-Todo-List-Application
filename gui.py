e04d655ca103986bfd9d9dafe10868a467217963
Charanvir <charanvir123@gmail.com>
2022-12-22 23:28:26 -0500

    added editing functionality, updating ListBox when new item is added, and when item is edited

--- gui.py
+++ gui.py
@@ -4,6 +4,12 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+todos = todo_functions.get_todos()
+list_box = sg.Listbox(values=todos,
+                      key="todos",
+                      enable_events=True,
+                      size=(45, 10))
+edit_button = sg.Button("Edit")
 
 exit_button = sg.Button("Exit")
 
@@ -14,6 +20,8 @@ window = sg.Window(
         [label],
         # Row 2
         [input_box, add_button],
+        # Row 3
+        [list_box, edit_button],
         # Exit Row
         [exit_button]
     ],
@@ -27,6 +35,16 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             todo_functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = todo_functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            todo_functions.write_todos(todos)
+            window["todos"].update(values=todos)
         case sg.WINDOW_CLOSED:
             break
         case "Exit":
